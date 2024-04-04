b09881d99fc91db60e63285b408fa0101f93c18a
theclassyguy2006 <140506001+theclassyguy2006@users.noreply.github.com>
2024-04-04 23:49:51 +0530

    Add files via upload

---
+++ project2-mars.ino
@@ -0,0 +1,38 @@
+#include <Adafruit_LiquidCrystal.h>
+
+Adafruit_LiquidCrystal lcd(0);
+int sensor;
+int threshold = 500;
+float smoke;
+
+void setup()
+{
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+}
+
+void loop()
+{
+  sensor = analogRead(A2);
+  smoke = analogRead(A0);
+  
+  if(sensor > threshold){
+    if(smoke >= 250){
+      lcd.print("Too much smoke");
+      delay(1000);
+      lcd.clear();
+    }
+    else{
+      lcd.print("Bad Orientation");
+      delay(1000);
+      lcd.clear();
+    }
+  }
+  else{
+  	lcd.print("Solar Panel fine!");
+    delay(1000);
+    lcd.clear();
+  }
+}
+
+
